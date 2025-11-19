7e484a6c0f53db7e0a649b0c9589356a6ac98a0a
Sergey <sergeyligay@gmail.com>
2025-11-19 06:04:37 +0300

    initial userscript

---
+++ myscript.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         My Tamper Script
+// @namespace    https://example.com/
+// @version      0.0.1
+// @description  Пример userscript — меняй в Antigravity, нажимай Deploy
+// @match        https://*/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/c00per01/tamper-myscript/main/myscript.user.js
+// @downloadURL  https://raw.githubusercontent.com/c00per01/tamper-myscript/main/myscript.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("My Tamper Script v0.0.1 loaded");
+    // ТВОЙ КОД ЗДЕСЬ
+})();
