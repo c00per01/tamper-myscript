4340a83a920e09600c16c71d361b6cd5d59d7f6a
Sergey <sergeyligay@gmail.com>
2025-11-19 09:29:34 +0300

    fix: restore @version to 0.0.4 (repair corrupted line)

--- myscript.user.js
+++ myscript.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         My Tamper Script
 // @namespace    https://example.com/
-// $10.0.4
+// @version      0.0.4
 // @description  Пример userscript — меняй в Antigravity, нажимай Deploy
 // @match        https://*/*
 // @grant        none
@@ -14,6 +14,7 @@
     console.log("My Tamper Script v0.0.2 loaded");
     // ТВОЙ КОД ЗДЕСЬ
 })();
+
 
 
 
