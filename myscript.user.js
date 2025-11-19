4b55ab62cefd6c82b2315dd3918e976a3c96cc26
Sergey <sergeyligay@gmail.com>
2025-11-19 08:20:06 +0300

    bump: v0.0.2

--- myscript.user.js
+++ myscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         My Tamper Script
 // @namespace    https://example.com/
-// @version      0.0.1
+// $10.0.2
 // @description  Пример userscript — меняй в Antigravity, нажимай Deploy
 // @match        https://*/*
 // @grant        none
