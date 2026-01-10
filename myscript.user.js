41e850c6e9d06892fec4f67d5e791bd76d748792
Sergey <sergeyligay@gmail.com>
2026-01-10 14:59:57 +0300

    smart-bump: v1.137.38

--- myscript.user.js
+++ myscript.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         My Tamper Script
 // @namespace    https://example.com/
-// @version 1.137.37
+// @version 1.137.38
 // @description  Пример userscript — меняй в Antigravity, нажимай Deploy
 // @match        https://*/*
 // @grant        none
