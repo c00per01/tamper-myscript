0ea9fc1b54fc63f0b735d04d41480e2e23a8eadb
Sergey <sergeyligay@gmail.com>
2026-01-16 08:25:02 +0300

    deploy: v1.193.5

--- myscript.user.js
+++ myscript.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         My Tamper Script
 // @namespace    https://example.com/
-// @version 1.193.4
+// @version 1.193.5
 // @description  Пример userscript — меняй в Antigravity, нажимай Deploy
 // @match        https://*/*
 // @grant        none
