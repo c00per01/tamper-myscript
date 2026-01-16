844dba59d57325e52794fa6b88b6ff1aec3d7856
Sergey <sergeyligay@gmail.com>
2026-01-16 08:19:53 +0300

    fix: restore HTML comments in placements module panel v1.193.3

--- content.js
+++ content.js
@@ -1,14 +1,4 @@
-﻿// ==UserScript==
-// @name         My Tamper Script
-// @namespace    https://example.com/
-// @version 1.193.3
-// @description  Пример userscript — меняй в Antigravity, нажимай Deploy
-// @match        https://*/*
-// @grant        none
-// @updateURL    https://raw.githubusercontent.com/c00per01/tamper-myscript/main/myscript.user.js
-// @downloadURL  https://raw.githubusercontent.com/c00per01/tamper-myscript/main/myscript.user.js
-// ==/UserScript==
-
+﻿
 (function () {
     'use strict';
 
